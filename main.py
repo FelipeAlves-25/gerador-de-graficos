24caa06685adbb9e29cf389597e7e7abffd5b910
Felipe Alves <felipefas209@outlook.com>
2025-06-10 13:18:19 -0300

    Estrutura inicial em desenvolvimento, menu completo porém sem a inserção das funções

---
+++ main.py
@@ -0,0 +1,12 @@
+from os import system
+
+
+
+while True:
+    print("Seja bem-vindo ao gerador de gráficos.\n\nPor favor, selecione a função desejada:\n")
+    print("[1] Novo gráfico\n[2] Carregar gráfico existente\n[3] Sair\n")
+    op = int(input("Digite aqui: "))
+
+    if op == 3:
+        print("Obrigado por utilizar meu programa. :)")
+        break
